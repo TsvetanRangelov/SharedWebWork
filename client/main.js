36642e51b0a074924f850f83b4dd8883d0ab6370
joelever <2079568l@student.gla.ac.uk>
2016-11-04 15:22:13 +0000

    fixed facebook login

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import '../imports/startup/accounts-config.js';
+import '../imports/startup/social-config.js';
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
---
+++ imports/startup/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: 'sharedWhiteboard',
+    secret: '2e2ecb21b8c0229f73e01b3595a8d483'
+});
