3bc11d309eee27af8be443217ce44821425089a7
joelever <2079568l@student.gla.ac.uk>
2016-11-04 16:27:43 +0000

    mohsin look

--- imports/startup/social-config.js
+++
@@ -1,9 +0,0 @@
-ServiceConfiguration.configurations.remove({
-    service: 'facebook'
-});
-
-ServiceConfiguration.configurations.insert({
-    service: 'facebook',
-    appId: 'sharedWhiteboard',
-    secret: '2e2ecb21b8c0229f73e01b3595a8d483'
-});
